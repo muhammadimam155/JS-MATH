3552bf00304dc6918aa00303fd5cefb35d5bf677
muhammadimam155 <imamaliatari@gmail.com>
2023-10-04 00:31:04 +0500

    math

---
+++ app.js
@@ -0,0 +1,73 @@
+var num1 = prompt("write the first number who you be sub  ")
+var num2 = prompt("write the second number")
+var sub = (num1 - num2)
+document.write ("<h2>sub of " + num1 + " and " + num2 + " is " + sub + " <br> </h2> ")
+
+
+
+var opr = prompt("chose your oppertor like + , -, * , /")
+var num1 = prompt("write the first number")
+var num2 = prompt("write the second number")
+if (opr == "+") {
+    document.write(num1 + num2)
+}
+else if (opr == "-") {
+    document.write(num1 - num2)
+}
+else if (opr == "*") {
+    document.write(num1 * num2)
+}
+ else if (opr == "/") {
+    document.write(num1 / num2)
+}
+else{
+    alert("please chose any opr or num ")
+}
+
+
+var num1 = 1
+var num2 = 2
+var num3 = 3
+var num4 = 4
+var num5 = 5
+var num6 = 6
+var num7 = 7
+var num8 = 8
+var num9 = 9
+var num10 = 10 
+
+document.write("<h1> table of 4 </h1>")
+
+if (num1 == "1"){
+    document.write("4 * 1 = "+4 * 1 +" <br>")
+}
+if (num2 == "2"){
+    document.write("4 * 2 = "+4 * 2 +" <br> ")
+}
+if (num3 == "3"){
+    document.write("4 * 3 = "+4 * 3 +" <br>")
+}
+if (num4 == "4"){
+    document.write("4 * 4 = "+4 * 4 +" <br>")
+}
+if (num5 == "5"){
+    document.write("4 * 5 = "+4 * 5 +"<br> ")
+}
+if (num6 == "6"){
+    document.write("4 * 6= "+4 * 6 +"<br>  ")
+}
+if (num7 == "7"){
+    document.write("4 * 7 = "+4 * 7 +"<br>  ")
+}
+if (num8 == "8"){
+    document.write("4 * 8 = "+4 * 8 +" <br> ")
+}
+if (num9 == "9"){
+    document.write("4 * 9 = "+4 * 9 +"<br> ")
+}
+if (num10 == "10"){
+    document.write("4 * 10 = "+4 * 10 +"<br>   ")
+}
+
+
+
